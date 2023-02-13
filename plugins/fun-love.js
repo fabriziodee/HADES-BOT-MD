c945fd9bdd11749243a3ccf8823215293211c520
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-13 11:02:09 +0100

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
-*El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+let love = `*❤️❤️ MISURATORE D'AMORE ❤️❤️*
+*L'amore di ${text} per te è di* *${Math.floor(Math.random() * 100)}%* *per un 100%*
+*Dovresti chiederle di essere la tua ragazza/o ?*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love']
